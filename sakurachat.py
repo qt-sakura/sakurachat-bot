be3169aabc15c5b86d65c176903f5c834a24bcea
qt-sakura <qtsakuraharuno@gmail.com>
2025-08-14 20:21:57 +0600

    Update sakurachat.py

--- sakurachat.py
+++ sakurachat.py
@@ -501,7 +501,7 @@ class ColoredFormatter(logging.Formatter):
 # Configure logging with colors
 def setup_colored_logging():
     """Setup colored logging configuration"""
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger("sakura")  # CHANGED: Use "sakura" instead of __name__
     logger.setLevel(logging.INFO)
     
     # Remove existing handlers
